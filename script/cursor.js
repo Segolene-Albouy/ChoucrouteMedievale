641cc79c241abbcd405d4a1f528c572016a42fd2
Biratus <clement.birette@hotmail.fr>
2024-01-15 19:21:33 +0100

    cursor particle pool & burst

--- script/cursor.js
+++ script/cursor.js
@@ -1,18 +1,17 @@
 const width = 30;
 const height = 52;
+const fps = 30;
+let hoverInterval = null; // Interval des particules quand element hover.
+let hoverAnimationRunning = false;
 
-const createParticleCursor = (x, y) => {
-  const cursor = document.createElement("span");
-  cursor.classList.add("custom-cursor");
-  cursor.classList.add("cursor-particle");
-  document.body.appendChild(cursor);
-
-  const randomSize = randomBetween(0.1, 0.8);
-
-  cursor.style.width = Math.floor(width * randomSize) + "px";
-  cursor.style.height = Math.floor(height * randomSize) + "px";
+const createParticleCursor = (x, y, size, rotation, duration) => {
+  const cursor = particlePool.getNewParticle(duration);
+  cursor.style.width = Math.floor(width * size) + "px";
+  cursor.style.height = Math.floor(height * size) + "px";
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
+  cursor.style.rotate = `${rotation}rad`;
+  cursor.style.opacity = 1;
   return cursor;
 };
 
@@ -34,45 +33,143 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.addEventListener("click", function (e) {
-    const particleNb = 60;
-    const { x, y } = mousePosition(e);
-    console.log({ x, y });
+    burst(50);
+  });
 
-    const particles = [];
+  const openDoorButton = document.querySelector("#opendor");
 
-    for (let i = 0; i < particleNb; i++) {
-      particles.push(spawnParticle(x, y));
+  openDoorButton.addEventListener("mousemove", function (e) {
+    hoverAnimationRunning = true;
+  });
+  openDoorButton.addEventListener("mouseleave", function (e) {
+    hoverAnimationRunning = false;
+  });
+
+  initIdleCursorAnimation();
+});
+
+function spawnParticle(x, y, options) {
+  let speed = options
+    ? options.speed || floorRandomBetween(2, 10)
+    : floorRandomBetween(2, 10);
+  let duration = options
+    ? options.duration || floorRandomBetween(500, 1500)
+    : floorRandomBetween(500, 1500);
+  let size = options ? options.size : undefined;
+  let direction = options
+    ? options.direction || { x: randomBetween(0, 1), y: randomBetween(0, 1) }
+    : { x: randomBetween(0, 1), y: randomBetween(0, 1) };
+  let rotation = options
+    ? options.rotation !== undefined
+      ? options.rotation
+      : randomBetween(0, Math.PI * 2)
+    : randomBetween(0, Math.PI * 2);
+
+  let follow = particlePool.pool.length == 0;
+  const particle = createParticleCursor(x, y, size, rotation, duration);
+  particle.direction = direction;
+  particle.speed = speed;
+  particle.duration = duration;
+  particle.follow = follow;
+
+  return particle;
+}
+
+function burst(nbParticle) {
+  const {
+    x: eltX,
+    y: eltY,
+    width,
+    height,
+  } = document.getElementById("main-cursor").getBoundingClientRect();
+
+  const particles = [];
+
+  for (let i = 0; i < nbParticle; i++) {
+    const randomX = randomBetween(-1, 1);
+    const randomY = randomBetween(-1, 1);
+
+    const x = eltX + width / 2 + Math.floor(randomX * width * 0.2);
+    const y = eltY + height / 2 + Math.floor(randomY * height * 0.2);
+
+    const ratio = Math.random();
+    const size = ratio * 1.5;
+    const speed = Math.floor(10 / ratio);
+    const duration = Math.floor(800 + ratio * 800);
+    particles.push(
+      spawnParticle(x, y, {
+        direction: { x: randomX, y: randomY },
+        size,
+        speed,
+        duration,
+        rotation: randomBetween(0, Math.PI * 2),
+      })
+    );
+  }
+}
+
+function initIdleCursorAnimation() {
+  hoverInterval = setInterval(() => {
+    // monitorParticles();
+    particlePool.getRunningParticles().forEach((particle) => {
+      const top = parseInt(particle.style.top.slice(0, -2));
+      const left = parseInt(particle.style.left.slice(0, -2));
+
+      const addedX = Math.round(particle.direction.x * particle.speed);
+      const addedY = Math.round(particle.direction.y * particle.speed);
+      particle.style.top = top + addedX + "px";
+      particle.style.left = left + addedY + "px";
+      particle.style.opacity =
+        parseFloat(particle.style.opacity) - fps / particle.duration;
+    });
+
+    document.getElementById("main-cursor").style.opacity = 1;
+    if (!hoverAnimationRunning) return;
+    document.getElementById("main-cursor").style.opacity = 0;
+    burst(2);
+  }, 1000 / fps);
+}
+
+const particlePool = {
+  pool: [],
+  getRunningParticles: function () {
+    return this.pool.filter((p) => p.state == "running").map((p) => p.particle);
+  },
+  getNewParticle: function (duration) {
+    const freeParticle = this.pool.find((p) => p.state == "ended");
+    let targetParticle;
+
+    if (freeParticle) {
+      freeParticle.state = "running";
+      freeParticle.particle.style.display = "block";
+      targetParticle = freeParticle;
+    } else {
+      const element = document.createElement("span");
+      element.classList.add("custom-cursor");
+      element.classList.add("cursor-particle");
+      document.body.appendChild(element);
+      const poolParticle = {
+        particle: element,
+        state: "running",
+        follow: this.pool.length == 0,
+      };
+      this.pool.push(poolParticle);
+      targetParticle = poolParticle;
     }
 
     setTimeout(() => {
-      particles.forEach((particle) => {
-        // particle.remove();
-        particle.particle.style.transform = particle.transform;
-        particle.particle.style.opacity = 0;
-      });
-      setTimeout(() => {
-        particles.forEach((particle) => {
-          particle.particle.remove();
-        });
-      }, 1500);
-    }, 1);
-  });
-});
+      targetParticle.particle.style.display = "none";
+      targetParticle.state = "ended";
+    }, duration);
+    return targetParticle.particle;
+  },
+};
 
-function spawnParticle(x, y) {
-  const maxDirection = 2;
-  const directionX = floorRandomBetween(maxDirection * -1, maxDirection);
-  const directionY = floorRandomBetween(maxDirection * -1, maxDirection);
-  const particle = createParticleCursor(x + directionX, y + directionY);
-
-  const speed = floorRandomBetween(2, 25);
-
-  const duration = floorRandomBetween(500, 1500);
-
-  particle.style.transition = "all " + duration + "ms";
-
-  return {
-    particle,
-    transform: `translate(${directionX * speed}px, ${directionY * speed}px)`,
-  };
+function monitorParticles() {
+  console.log(
+    "Particle running " +
+      particlePool.getRunningParticles().length +
+      "/" +
+      particlePool.pool.length
+  );
 }
