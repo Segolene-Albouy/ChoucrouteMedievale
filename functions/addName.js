cc8a9d137f75ce23d7f4cd3d0eef4bbafbc5637d
Segolene <segolene.albouy@gmail.com>
2024-02-01 16:10:41 +0100

    [LANDING] API connection

--- functions/addName.js
+++ functions/addName.js
@@ -21,28 +21,6 @@
 const functions = require('@google-cloud/functions-framework');
 const { Firestore } = require('@google-cloud/firestore');
 
-const castes = [
-    "noble",
-    "chevalier",
-    "artisan",
-    "paysan"
-];
-
-const teams = {
-    corbeau: {
-        color: "black"
-    },
-    cerf: {
-        color: "green"
-    },
-    kraken: {
-        color: "blue"
-    },
-    dragon: {
-        color: "red"
-    }
-};
-
 const firestore = new Firestore({
     projectId: 'choucroutemedievale',
     databaseId: 'choucroute-dev'
@@ -51,68 +29,128 @@ const firestore = new Firestore({
 const gueuxDb = firestore.collection('names');
 const mdpDb = firestore.collection('psw');
 
-functions.http('setName', async (req, res) => {
-    const domain = req.headers.referer;
-    if (!domain || !domain.includes('choucroute-medievale.tech')) {
-        res.status(403).send(`Que croyais-tu, manand ?`);
-        return;
-    }
+async function newPsw () {
+    const freePsw = await mdpDb.where('attributed', '==', false).get();
+    return freePsw.docs[0].id;
+}
 
+async function newGueux (name, psw, ipAddress, now) {
+    const gueuxData = {
+        name: name,
+        team: "",
+        created: now,
+        psw: psw,
+        lastConnection: now,
+        ips: [ipAddress]
+    };
+
+    await gueuxDb.doc(gueuxDb.doc().id).set(gueuxData);
+    return gueuxData
+}
+
+async function updateGueux (gueux, ipAddress, now) {
+    const gueuxData = gueux.data();
+    await gueux.ref.update({ lastConnection: now });
+    if (!gueuxData.ips || !gueuxData.ips.includes(ipAddress)) {
+        await gueux.ref.update({ ips: Firestore.FieldValue.arrayUnion(ipAddress) });
+    }
+    return gueuxData;
+}
+
+functions.http('setName', async (req, res) => {
+    let { name, psw } = req.body; // GET req.query / POST req.body
+
+    /* WARNING ONLY FOR DEVELOPMENT */
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    /*if (name === "Segoline La Devergoigneuse"){
+        res.status(201).json({
+            greetings: `Hommage à toi, noble ${name} !`,
+        });
+    } else {
+        const domain = req.headers.referer;
+        if (!domain || !domain.includes('choucroute-medievale.tech')) {
+            res.status(403).send(`Que croyais-tu, manand ?`);
+            return;
+        }
+    }*/
+
+    let status;
     try {
         const now = Firestore.FieldValue.serverTimestamp();
-
-        // GET req.query
-        // POST req.body
-        const { name, psw } = req.body;
         const ipAddress = req.ip;
 
+        // The user submits a name
         if (name){
             const nameQuery = await gueuxDb.where('name', '==', name).get();
             // const ipQuery = await gueuxDb.where('ips', 'array-contains', ipAddress).get();
 
-            let status;
             if (nameQuery.size === 1) {
-                const gueuxDoc = nameQuery.docs[0];
-                const gueuxData = gueuxDoc.data();
-
-                await gueuxDoc.ref.update({ lastConnection: now });
-                if (!gueuxData.ips || !gueuxData.ips.includes(ipAddress)) {
-                    await gueuxDoc.ref.update({ ips: Firestore.FieldValue.arrayUnion(ipAddress) });
-                }
-
-                status = "update";
+                // the name is already taken
+                status = "taken"
             } else if (nameQuery.size > 1) {
+                // tha name was attributed multiple time (should not happen)
                 status = "wtf";
-                res.status(201).json({
-                    greetings: `Bienvenue à toi, ${name}!`,
-                    ip: ipAddress,
-                    status
-                });
             } else {
-                // Name not associated, create a new gueux profile
-                const newGueux = {
-                    name: name,
-                    team: "",
-                    created: now,
-                    lastConnection: now,
-                    ips: [ipAddress]
-                };
-
-                await gueuxDb.doc(gueuxDb.doc().id).set(newGueux);
+                // name not taken, create a new gueux profile
+                psw = newPsw();
+                await newGueux(name, psw, ipAddress, now)
                 status = "add";
+            }
+        } else if (psw){
+            // the user tries a psw
+            const pswQuery = await gueuxDb.where('psw', '==', psw).get();
+
+            if (pswQuery.size === 1) {
+                // the password is correct
+                status = "update"
+                await updateGueux(pswQuery.docs[0], ipAddress, now);
+            } else if (pswQuery.size > 1) {
+                // that psw was attributed multiple time (should not happen)
+                status = "wtf";
+            } else {
+                // incorrect password
+                status = "incorrect";
             }
         }
 
-
-
-        // res.send(`Bienvenue à toi, ${name}!`);
         res.status(201).json({
-            greetings: `Bienvenue à toi, ${name}!`,
-            ip: ipAddress,
+            name,
+            psw,
             status
         });
     } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).json({
+            error,
+            status: "error",
+            name: null,
+            psw: null
+        });
     }
 });
+
+/**
+ * const castes = [
+ *     "noble",
+ *     "chevalier",
+ *     "artisan",
+ *     "paysan"
+ * ];
+ *
+ * const teams = {
+ *     corbeau: {
+ *         color: "black"
+ *     },
+ *     cerf: {
+ *         color: "green"
+ *     },
+ *     kraken: {
+ *         color: "blue"
+ *     },
+ *     dragon: {
+ *         color: "red"
+ *     }
+ * };
+ */
--- script/submitName.js
+++ script/submitName.js
@@ -1,5 +1,40 @@
 var errorMessageElement = null;
 var wrongOnce = false;
+var APIurl = "https://europe-west9-choucroutemedievale.cloudfunctions.net/checkName";
+var pswAttempts = null;
+
+function choose(rpgBtn, callback){
+  rpgBtn.parentElement.remove();
+  callback();
+}
+
+function resetLocalStorage(){
+  localStorage.removeItem("pswAttempts");
+  localStorage.removeItem("medievalPsw");
+  localStorage.removeItem("medievalName");
+}
+
+function passePartout(){
+  localStorage.setItem("medievalName", "Segoline La Devergoigneuse");
+  localStorage.setItem("medievalPsw", "Je suis ton maitre");
+}
+
+function displayError(msg=null){
+  if (msg){
+    errorMessageElement.querySelector('span').innerHTML = msg;
+  }
+
+  if (wrongOnce) {
+    // Abusé
+    errorMessageElement.classList.add("again");
+    setTimeout(() => {
+      errorMessageElement.classList.remove("again");
+    }, 600);
+  } else {
+    wrongOnce = true;
+    errorMessageElement.style.display = "flex";
+  }
+}
 
 function submitName(evt) {
   evt.preventDefault();
@@ -7,70 +42,97 @@ function submitName(evt) {
   const name = evt.target.name ? evt.target.name.value : null;
   const adj = evt.target.adjectif ? evt.target.adjectif.value : null;
   if (!name) {
-    if (wrongOnce) {
-      // Abusé
-      errorMessageElement.classList.add("again");
-      setTimeout(() => {
-        errorMessageElement.classList.remove("again");
-      }, 600);
-    } else {
-      wrongOnce = true;
-      errorMessageElement.style.display = "flex";
-    }
+    displayError();
     return;
   }
-
   errorMessageElement.style.display = "none";
-  const nameTaken = false; // TODO check if name is taken with API
-  if (nameTaken) {
-    document.getElementById("name_taken").style.display = "flex";
-  } else {
-    localStorage.setItem("medievalName", `${name} ${adj}`);
-    document.getElementById("landing-name").classList.add("submitted");
-    // TODO Save to API
-  }
+  let medievalName = `${name} ${adj}`;
+  retrieveJSON(
+      APIurl, { name: medievalName }
+  ).then(res => {
+    console.log(res);
+  }).catch(res => {
+    // WEIRD: the response is caught here
+    res = JSON.parse(res.response);
+    if (res.hasOwnProperty("error")){
+      console.log(res.error);
+      displayError("?? Quelle est cette sorcellerie ??");
+      document.getElementById("name-submit").remove();
+      return
+    }
+    console.log(res);
+    switch (res.status) {
+      case 'taken':
+        // todo user already taken
+        document.getElementById("name-taken").style.display = "flex";
+        break;
+      case 'add':
+        // todo handle what if no psw and/or no name
+        localStorage.setItem("medievalPsw", res.psw);
+        localStorage.setItem("medievalPsw", res.name);
+        openGates(res.name, true);
+        break;
+      case 'wtf':
+      default:
+        console.log("should not happen");
+        displayError("?? Quelle est cette sorcellerie ??");
+        document.getElementById("psw-submit").remove();
+    }
+  });
 }
 
-function iChoose(elId){
-
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const medievalName = localStorage.getItem("medievalName");
-
-  /*if (medievalName) {
-    console.log({ medievalName });
-    document.getElementById("landing-name").remove();
-
-    retrieveJSON(
-        "https://europe-west9-choucroutemedievale.cloudfunctions.net/checkName",
-        { name: medievalName }
-    ).then(res => {
-      res = JSON.parse(res).body;
-
-      /!*if (!output && !isArray(output)){
-        errMsg(output, data, "Result output of Fill by model function is not correctly formatted");
-        return;
-      }*!/
-    }).catch(e => {
-      console.table(JSON.parse(e.response).detail);
-    });
-
-    // TODO save to API
-    // TODO allow say "c'est pas moi"
-
-    // document.getElementById("ask-name").innerHTML = `<h1>Bienvenue <mark>${medievalName}</mark></h1>`;
+function submitPsw(evt) {
+  evt.preventDefault();
+  pswAttempts++;
+  localStorage.setItem("pswAttempts", `${pswAttempts}`);
+  if (pswAttempts > 3){
+    displayError("!! Degouspillez sale maraud !!");
+    document.getElementById("psw-submit").remove();
+    // todo add: demande aux seigneurs
     return;
-  }*/
-  const pswAttempts = localStorage.getItem("pswAttempts");
-  if (pswAttempts){
-    localStorage.setItem("pswAttempts", "0");
-    // TODO do not show name / only mdp
   }
 
-  document.getElementById("ask-name").style.display = "block";
+  const medievalPsw = evt.target.psw ? evt.target.psw.value : null;
+  console.log(medievalPsw);
 
-  errorMessageElement = document.getElementById("errorMessage");
+  retrieveJSON(
+      APIurl, { psw: medievalPsw }
+  ).then(res => {
+    console.log(res);
+  }).catch(res => {
+    // WEIRD: the response is caught here
+    res = JSON.parse(res.response);
+    if (res.hasOwnProperty("error")){
+      console.log(res.error);
+      displayError("?? Essaye tu de nous tromper maraud ??");
+      document.getElementById("psw-submit").remove();
+      return
+    }
+    console.log(res);
+    switch (res.status) {
+      case 'update':
+        // todo handle what if no psw and/or no name
+        localStorage.setItem("medievalPsw", res.psw);
+        localStorage.setItem("medievalPsw", res.name);
+        openGates(res.name, true);
+        break;
+      case 'incorrect':
+        displayError();
+        break;
+      case 'wtf':
+      default:
+        console.log("should not happen");
+        displayError("?? Quelle diablerie avait vous faist ??");
+        document.getElementById("psw-submit").remove();
+    }
+  });
+}
+
+function showName(){
+  document.getElementById("douves").remove();
+  document.getElementById("form-name").style.display = "block";
+
+  errorMessageElement = document.getElementById("error-name");
 
   // Build adjectifs options
   const select = document.querySelector("#ask-name select");
@@ -87,13 +149,58 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   select.children[0].selected = true;
+}
+
+function showPsw(){
+  document.getElementById("douves").remove();
+  document.getElementById("form-psw").style.display = "block";
+  errorMessageElement = document.getElementById("error-psw");
+}
+
+function openGates(medievalName, submitted= false){
+  document.getElementById("opendor").innerHTML = `Pénétrez au chateau<mark>${medievalName}</mark>`;
+  if (submitted){
+    document.getElementById("landing-name").classList.add("submitted");
+  } else {
+    document.getElementById("landing-name").remove();
+  }
+
+  // ici si un ptit filou a ajouté un medievalName + un medievalPsw dans son localStorage, il peut entrer
+  // en revanche, il n'aura jamais de clans s'il fait ça 🤨
+
+  // TODO allow say "je veux changer de nom"
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const medievalName = localStorage.getItem("medievalName");
+  const medievalPsw = localStorage.getItem("medievalPsw");
+
+  if (medievalName && medievalPsw) {
+    // ça passe les gardes
+    openGates(medievalName);
+    // API call to update last connection
+    retrieveJSON(
+        APIurl, { psw: medievalPsw }
+    ).catch(res => {
+      console.log(JSON.parse(res.response));
+    });
+    return;
+  }
+  // TODO what about y'a un nom mais pas de mdp ??
+  if (localStorage.getItem("pswAttempts")){
+    // if the user already tried to connect with psw before, reset their attempts
+    pswAttempts = 0;
+    document.getElementById("choose-name").remove();
+    document.getElementById("douves").innerHTML = `<h3>Halte-là !</h3>
+      <p>Je reconnois vostre visage.</p><p>Quel est vostre mot de passe ?</p>`;
+  }
+  // les gardes apparaissent
+  document.getElementById("ask-name").style.display = "block";
 });
 
-/*
-
-ADJECTIFS
-
-*/
+/**
+ * ADJECTIFS
+ */
 const adjectifs = [
   ["Le Pieu", "La Pieuse"],
   ["L’Affreux", "L’Affreuse"],
@@ -149,17 +256,3 @@ const adjectifs = [
   ["L’Audicieux", "L’Audicieuse"],
   ["Le Dangereux", "La Dangereuse"],
 ];
-
-/*
-Ont été mentionnés mais pas ajouté (je me dis ça va pas trop dans le thème ou autre..):
-L'enculé
-Le casanié
-L'ivrogne (tout le monde voudra être l'ivrogne)
-Le bleu
-Le pubert
-Le Zelé
-Le puant
-// Et la copilot me propose "Le puceau" AHA
-// en vrai je pense qu'on pourra dédoublonner lâche/couard, vaillant/valeureux ?
-*/
-
