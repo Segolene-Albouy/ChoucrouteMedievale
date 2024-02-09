c7432040f9d5c53550ed7542896c6c0a0b76ebaf
Segolene <segolene.albouy@gmail.com>
2024-02-10 00:07:31 +0100

    [PAGE] majestueux lookbook à terminer

---
+++ script/lookbook.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll(".drag img");
+    images.forEach(dragElement);
+
+    function dragElement(img) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        let originalZIndex = 1; // Store the original z-index
+
+        img.onmousedown = dragMouseDown;
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // originalZIndex = img.style.zIndex || 1; // Store the original z-index
+            img.style.zIndex = img.style.zIndex + 1; // Set a high z-index to bring the element to the top
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            img.style.top = (img.offsetTop - pos2) + "px";
+            img.style.left = (img.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // img.style.zIndex = originalZIndex;
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+});
